1a6f3054dacd54df3e97709be0fcef73b2506f3b
Xen0mrefne <x3n0g3n1c@gmail.com>
2024-02-14 15:30:30 -0300

    router set up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: "calendar", component: CalendarComponent},
+    {path: "about", component: AboutComponent},
+    {path: "", redirectTo: "/calendar", pathMatch: "full"}
+];
